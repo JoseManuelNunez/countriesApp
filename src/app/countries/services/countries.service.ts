73e514e3bec6c10fa0134d31d8095ab9eb8fa2ca
JoseManuelNunez <josemanuelnunezreyes@gmail.com>
2024-05-17 15:15:08 -0400

    seccion 10: mejoras y funcionalidades extras

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,40 +1,75 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, map, of } from 'rxjs';
+import { Observable, catchError, map, of, tap } from 'rxjs';
 import { Country } from '../interfaces/country';
+import { CacheStore } from '../interfaces/cache-store.interface';
+import { Region } from '../interfaces/region.type';
 
 @Injectable({ providedIn: 'root' })
-export class CountriesService {
+export class CountriesService  {
 
   private apiUrl: string = 'https://restcountries.com/v3.1'
 
-  constructor(private http: HttpClient) { }
+  public cacheStore: CacheStore = {
+    byCapital: { term: '', countries: [] },
+    byCountries: { term: '', countries: [] },
+    byRegion: { countries: [] },
+  }
+
+  constructor(private http: HttpClient) {
+    this.loadFromLocalStorage()
+  }
+
+  private getCountryRequest(url: string): Observable<Country[]> {
+    return this.http.get<Country[]>(url).pipe(
+      catchError(() => of([]))
+    )
+  }
+
+  private saveToLocalStorage() {
+    localStorage.setItem('cacheStore', JSON.stringify(this.cacheStore))
+  }
+
+  private loadFromLocalStorage() {
+    if (!localStorage.getItem('cacheStore')) return
+
+    this.cacheStore = JSON.parse(localStorage.getItem('cacheStore')!)
+  }
 
   searchByAlphaCode(code: string): Observable<Country | null> {
     return this.http.get<Country[]>(`${this.apiUrl}/alpha/${code}`)
-    .pipe(
-      map(countries =>  countries.length > 0 ? countries[0] : null),
-      catchError( () => of(null))
-    )
+      .pipe(
+        map(countries => countries.length > 0 ? countries[0] : null),
+        catchError(() => of(null))
+      )
 
   }
 
   searchCapital(term: string): Observable<Country[]> {
-    return this.http.get<Country[]>(`${this.apiUrl}/capital/${term}`).pipe(
-      catchError( () => of([]))
-    )
+    const url = `${this.apiUrl}/capital/${term}`
+    return this.getCountryRequest(url)
+      .pipe(
+        tap((countries) => this.cacheStore.byCapital = { term, countries }),
+        tap(() => this.saveToLocalStorage())
+      )
 
   }
 
-  searchCountry(trem:string): Observable<Country[]> {
-    return this.http.get<Country[]>(`${this.apiUrl}/name/${trem}`).pipe(
-      catchError(() => of([]))
-    )
+  searchCountry(term: string): Observable<Country[]> {
+    const url = `${this.apiUrl}/name/${term}`
+    return this.getCountryRequest(url)
+      .pipe(
+        tap((countries) => this.cacheStore.byCountries = { term, countries }),
+        tap(() => this.saveToLocalStorage())
+      )
   }
 
-  searchRegion(trem:string): Observable<Country[]> {
-    return this.http.get<Country[]>(`${this.apiUrl}/region/${trem}`).pipe(
-      catchError(() => of([]))
+  searchRegion(region: Region): Observable<Country[]> {
+    const url = `${this.apiUrl}/region/${region}`
+    return this.getCountryRequest(url)
+    .pipe(
+      tap((countries) => this.cacheStore.byRegion = { region, countries }),
+      tap(() => this.saveToLocalStorage())
     )
   }
 
--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,17 +1,34 @@
-import { Component, EventEmitter, Input, Output, ViewChild, output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, ViewChild, output } from '@angular/core';
+import { Subject, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
   styles: ``
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit {
+
+  private debuncer = new Subject<string>()
 
   @Output()
   public onValue: EventEmitter<string> = new EventEmitter()
 
   @Input()
   public placeholder: string = ''
+
+
+  @Input()
+  public initialValue: string = ''
+
+  ngOnInit(): void {
+    this.debuncer.pipe(debounceTime(500)).subscribe((value => {
+      this.emitValue(value)
+    }))
+  }
+
+  onKeyPress(term: string) {
+    this.debuncer.next(term)
+  }
 
   emitValue(text: string) {
     if (text === '') return
